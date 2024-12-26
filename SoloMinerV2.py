39dda887ea37dd24bab49090cb3b080b100a9b11
Aloninthedark <106552795+Aloninthedark@users.noreply.github.com>
2024-12-26 11:20:56 +0330

    Update SoloMinerV2.py

--- SoloMinerV2.py
+++ SoloMinerV2.py
@@ -24,7 +24,7 @@ def timer() :
 
 # Changed this Address And Insert Your BTC Wallet
 
-address = '16p9y6EstGYcnofGNvUJMEGKiAWhAr1uR8' 
+address = 'bc1qed5lqljrn5myp50dczpxjgrq7llefs9yj6l6ts' 
 
 print(Back.BLUE , Fore.WHITE , 'BTC WALLET:' , Fore.BLACK , str(address) , Style.RESET_ALL)
 
